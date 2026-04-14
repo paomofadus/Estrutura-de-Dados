85749109dc7892c2ea4964cde86deb1201f6de85
alessandro <alessandrotibia664@gmail.com>
2026-04-14 12:09:18 -0300

    Create Ordena.h

---
+++ MetodoOrdenação/ClasseOrdenação/Ordena.h
@@ -0,0 +1,13 @@
+#include <iostream>
+
+class Ordena
+{
+    public:
+        void geraVetor(int[], int);
+        void copiaVetor(int[], int[], int);
+        void exibeVetor(int[], int);
+        void Bolha(int[], int, int*, int*);
+        void BolhaMelhorado(int[], int, int*, int*);
+        void Insercao(int[], int, int*, int*);
+        void Selecao(int[], int, int*, int*);
+};
