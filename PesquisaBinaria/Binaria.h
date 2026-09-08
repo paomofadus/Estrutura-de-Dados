28594eac876620718363261434cbcff795f4d9e3
Alessandro Moreira <alessandrotibia664@gmail.com>
2026-09-08 11:49:49 -0300

    Create Binaria.h

---
+++ PesquisaBinaria/Binaria.h
@@ -0,0 +1,11 @@
+#include <iostream>
+
+class Binaria
+{
+    public:
+        int PBIterativa(int [], int, int);
+        int PBRecursiva(int [], int, int, int);
+        int PBInicio(int [], int, int, int);
+        int PBFinal(int [], int, int, int);
+        void Ordena(int [], int);
+};
